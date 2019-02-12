45a68f39120e12ae8834004c674dec9e9970ad35
Maks <max.maisak@gmail.com>
2019-02-12 14:29:55 +0100

    Level: put a player avatar on the grid

--- assets/scripts/scenes/level.lua
+++ assets/scripts/scenes/level.lua
@@ -4,6 +4,8 @@
 -- Time: 11:59
 --
 
+require('assets/scripts/vector')
+
 local gridSize = {x = 10, y = 10 }
 local grid = {} -- grid[x][y] is a table that describes the tile at integer coordinates x and y
 for x = 1,gridSize.x do
@@ -12,6 +14,9 @@ for x = 1,gridSize.x do
         grid[x][y] = {}
     end
 end
+
+local playerStartPosition = {x = 2, y = 3}
+local player
 
 local tileMaterial = Game.makeMaterial {
     shininess = 100
@@ -27,7 +32,7 @@ local function makeTile(x, y, tile)
         z = (yNorm - 0.5) * gridSize.y
     }
 
-    Game.makeActor {
+    return Game.makeActor {
         Name = "Tile."..x.."."..y,
         Transform = {
             position = position,
@@ -38,6 +43,10 @@ local function makeTile(x, y, tile)
             material = tileMaterial
         }
     }
+end
+
+local function getPlayerPositionFromGridPosition(gridPosition)
+    return Vector.from {0, 1, 0} + grid[gridPosition.x or 1][gridPosition.y or 1].tile:get("Transform").position
 end
 
 local scene = {}
@@ -67,10 +76,43 @@ function scene.start()
     -- make views
     for x = 1,gridSize.x do
         for y = 1,gridSize.y do
-            print(x.."."..y)
-            makeTile(x, y, grid[x][y])
+            grid[x][y].tile = makeTile(x, y, grid[x][y])
         end
     end
+
+    local playerActor = Game.makeActor {
+        Name = "Player",
+        Transform = {
+            position = getPlayerPositionFromGridPosition(playerStartPosition),
+            scale = {0.3, 0.5, 0.3}
+        },
+        RenderInfo = {
+            mesh = "models/cube_flat.obj",
+            material = {
+                diffuseColor = {0.5, 0, 0}
+            }
+        }
+    }
+
+    player = {
+        actor = playerActor,
+        transform = playerActor:get("Transform"),
+        gridPosition = {x = playerStartPosition.x, y = playerStartPosition.y}
+    }
+end
+
+local wasUpPressed = false
+
+function scene.update()
+
+    if not wasUpPressed and Game.keyboard.isDown("up") then
+        if player.gridPosition.y < gridSize.y then
+            player.gridPosition.y = player.gridPosition.y + 1
+            player.transform.position = getPlayerPositionFromGridPosition(player.gridPosition)
+        end
+    end
+
+    wasUpPressed = Game.keyboard.isDown("up")
 end
 
 return scene

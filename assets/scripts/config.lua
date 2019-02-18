bfd05b3d38fe70bdf734a6f9c26c6be973f92f04
Maks <max.maisak@gmail.com>
2019-02-18 12:45:04 +0100

    UI/Text/Test: added null checks to the test code for viewing font atlases.

--- assets/scripts/config.lua
+++ assets/scripts/config.lua
@@ -4,7 +4,7 @@ width  = 800
 height = 600
 fullscreen = false
 framerateCap = 240
-startScene = 'assets/scripts/scenes/test/testUI.lua'
+startScene = 'assets/scripts/scenes/mainMenu.lua'
 firstLevelDefinition = 'assets/scripts/scenes/definition1.lua'
 
 -- custom

100966bcd78ef094ac406fb7a446c19de34b54f1
Popa <georgeantonio1998@yahoo.com>
2019-02-18 16:32:01 +0200

    UI : choose level functionality

--- assets/scripts/scenes/mainMenu.lua
+++ assets/scripts/scenes/mainMenu.lua
@@ -1,8 +1,18 @@
 require ('assets/scripts/level/level')
 
+local isBlocked = false
+local isChooseLevelOpened = false
+local mainButtonPanel
+local chooseLevelPanel
+local levelIndex = 1
+local numberOfLevels = 2
+
+
+
+
 local scenery = {
 	{
-		Name = "camera",
+		Name = "Camera",
 		Transform = {
 			position = { x = 0, y = 0, z = 10 }
 		},
@@ -16,8 +26,9 @@ function scene:start()
 	
 	Game.makeActors(scenery)
 
+--Start Main Buttons Panel
    	Game.makeActor {
-		Name = "ButtonsPanel",
+		Name = "MainButtonsPanel",
 		Transform = {},
 		UIRect = {
 			anchorMin	= {0.4, 0.15},
@@ -28,8 +39,13 @@ function scene:start()
 	Game.makeActor {
 		Name = "StartButton",
 		Transform = {
-			parent = "ButtonsPanel"
+			parent = "MainButtonsPanel"
 		},
+		Text = {
+			font = "fonts/Menlo.ttc",
+			color = {0, 0, 0, 1},
+			string = "START"
+        },
 		UIRect = {
 			anchorMin	= {0, 0.85},
 			anchorMax	= {1, 1}
@@ -37,13 +53,12 @@ function scene:start()
 		Sprite = {
 			material = {
 				shader		= "sprite",
-				texture		= "textures/runicfloor.png"
+				texture		= "textures/button.png"
 			}
 		},
 		LuaBehavior = {
 			onMouseDown = function(self, button)
 				if button == 1 then
-					print("start")
 					Game.loadScene(Level:new {
 						definitionPath = Config.firstLevelDefinition
 					})
@@ -53,10 +68,15 @@ function scene:start()
 	}
 
 	Game.makeActor {
-		Name = "ChoseLevelButton",
+		Name = "ChooseLevelButton",
 		Transform = {
-			parent = "ButtonsPanel"
+			parent = "MainButtonsPanel"
 		},
+		Text = {
+			font = "fonts/Menlo.ttc",
+			color = {0, 0, 0, 1},
+			string = "CHOOSE LEVEL"
+        },
 		UIRect = {
 			anchorMin = {0, 0.65},
 			anchorMax = {1, 0.8}
@@ -64,13 +84,15 @@ function scene:start()
 		Sprite = {
 			material = {
 				shader	= "sprite",
-				texture = "textures/runicfloor.png"
+				texture = "textures/button.png"
 			}
 		},
 		LuaBehavior = {
 			onMouseDown = function(self, button)
 				if button == 1 then
-					Game.find("ButtonsPanel"):get("UIRect").isEnabled	= false
+					isChooseLevelOpened = true
+					mainButtonPanel:get("UIRect").isEnabled		= false
+					chooseLevelPanel:get("UIRect").isEnabled	= true
 				end
 			end
 		}
@@ -79,7 +101,7 @@ function scene:start()
 	Game.makeActor {
 		Name = "CreditsButton",
 		Transform = {
-			parent = "ButtonsPanel"
+			parent = "MainButtonsPanel"
 		},
 		UIRect = {
 			anchorMin = {0, 0.45},
@@ -88,22 +110,22 @@ function scene:start()
 		Sprite = {
 			material = {
 				shader	= "sprite",
-				texture = "textures/runicfloor.png"
+				texture = "textures/button.png"
 			}
 		},
 		LuaBehavior = {
 			onMouseDown = function(self, button)
 				if button == 1 then
-					Game.find("ButtonsPanel"):get("UIRect").isEnabled		= false
+					mainButtonPanel:get("UIRect").isEnabled	= false
 				end
 			end
 		}
 	}
 
 	Game.makeActor {
-		Name = "Exitutton",
+		Name = "ExitButton",
 		Transform = {
-			parent = "ButtonsPanel"
+			parent = "MainButtonsPanel"
 		},
 		UIRect = {
 			anchorMin = {0, 0.25},
@@ -112,17 +134,105 @@ function scene:start()
 		Sprite = {
 			material = {
 				shader	= "sprite",
-				texture = "textures/runicfloor.png"
+				texture = "textures/button.png"
 			}
 		},
 		LuaBehavior = {
 			onMouseDown = function(self, button)
 				if button == 1 then
-					Game.find("ButtonsPanel"):get("UIRect").isEnabled		= false
+					mainButtonPanel:get("UIRect").isEnabled	= false
 				end
 			end
 		}
 	}
+--End Main Buttons Panel
+
+--Start ChoseLevel Panel
+	
+	Game.makeActor {
+		Name = "ChooseLevelPanel",
+		Transform = {},
+		UIRect = {
+			anchorMin = {0.2, 0.2},
+			anchorMax = {0.8, 0.8}
+		},		
+		Sprite = {
+			material = {
+				shader	= "sprite",
+				texture = "textures/level" .. levelIndex .. ".jpg"
+			}
+		},
+		LuaBehavior = {
+			update = function(self, dt)
+				if(isChooseLevelOpened) then
+					if(levelIndex < numberOfLevels) then
+						if Game.keyboard.isDown("right") then
+							print("is pressed right")
+							levelIndex = levelIndex + 1
+						
+							if(levelIndex > numberOfLevels) then
+								levelIndex = numberOfLevels
+							end
+
+							self.actor:remove("Sprite")
+							self.actor:add("Sprite", {
+								material = {
+									shader	= "sprite",
+									texture = "textures/level" .. levelIndex .. ".jpg"
+								}
+							})
+						end
+					end
+					
+					if(levelIndex > 1) then
+						if Game.keyboard.isDown("left") then
+							print("is pressed left")
+							levelIndex = levelIndex - 1
+
+							if(levelIndex < 1) then
+								levelIndex = 1
+							end
+
+							self.actor:remove("Sprite")
+							self.actor:add("Sprite", {
+								material = {
+									shader	= "sprite",
+									texture = "textures/level" .. levelIndex .. ".jpg"
+								}				
+							})
+						end
+					end
+				end
+			end
+		}
+	}
+
+	Game.makeActor {
+		Name = "BackButtonChoseLevel",
+		Transform = {
+			parent = "ChooseLevelPanel"
+		},
+		UIRect = {
+			anchorMin = {0.35, -0.25},
+			anchorMax = {0.65, -0.05}
+		},
+		Sprite = {
+			material = {
+				shader	= "sprite",
+				texture = "textures/button.png"
+			}
+		},
+		LuaBehavior = {
+			onMouseDown = function(self, button)
+				if button == 1 then
+					isChooseLevelOpened = false
+					chooseLevelPanel:get("UIRect").isEnabled	= false
+					mainButtonPanel:get("UIRect").isEnabled		= true
+				end
+			end
+		}
+	}
+--End ChoseLevel Panel
 
 	Game.makeActor {
 		Name = "MainPanel",
@@ -138,6 +248,11 @@ function scene:start()
 			}
 		}
 	}
+
+	mainButtonPanel = Game.find("MainButtonsPanel")
+	chooseLevelPanel = Game.find("ChooseLevelPanel")
+
+	chooseLevelPanel:get("UIRect").isEnabled	= false
 end
 
 return scene

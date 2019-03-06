d276583f3dcba882648d68f06efa970012e06bf1
Maks <max.maisak@gmail.com>
2019-03-06 23:38:02 +0100

    Rendering/Shadows: faster directional light PCF

--- assets/shaders/pbr.fs
+++ assets/shaders/pbr.fs
@@ -261,10 +261,10 @@ float CalculateDirectionalShadowMultiplier(int i, float biasMultiplier) {
     float shadow = 0.0;
     vec3 texelSize = 1.0 / textureSize(directionalDepthMaps, 0);
     vec2 scale = texelSize.xy * (1 + currentDepth * 0.1);
-    for (int x = -2; x <= 2; ++x)
-        for (int y = -2; y <= 2; ++y)
+    for (float x = -1.5; x <= 1.5; x += 1)
+        for (float y = -1; y <= 1.5; y += 1)
             shadow += texture(directionalDepthMaps, vec4(projected.xy + vec2(x, y) * scale, i, currentDepth - bias));
-    shadow /= 25.0;
+    shadow /= 16.0;
 
     return shadow;
 }

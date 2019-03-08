4cb0dd74eb471560d4305e62c29ed374e62556fa
Popa <georgeantonio1998@yahoo.com>
2019-03-08 16:08:06 +0200

    UI/Hud: Fix arrows offset to match the center of the parent

--- assets/scripts/UI/hudArrows.lua
+++ assets/scripts/UI/hudArrows.lua
@@ -46,14 +46,14 @@ function HudArrows:createArrows()
 		Name = "ArrowsPanel",
 		Transform = {},
 		UIRect = {
-			anchorMin = {0.1, 0.9},
-			anchorMax = {0.1, 0.9}
+			anchorMin = {0.1, 0.8},
+			anchorMax = {0.1, 0.8}
 		},
 	}
 
-	self.arrows["up"].actor  = createArrow({0,0,0},{-40,-40},{40,40})
-	self.arrows["left"].actor  = createArrow({0,0,90},{-80,-80},{0,0})
-	self.arrows["down"].actor  = createArrow({0,0,180},{-40,-120},{40,-40})
-	self.arrows["right"].actor  = createArrow({0,0,-90},{0,-80},{80,0})
+	self.arrows["up"].actor  = createArrow({0,0,0},{-40,0},{40,80})
+	self.arrows["left"].actor  = createArrow({0,0,90},{-80,-40},{0,40})
+	self.arrows["down"].actor  = createArrow({0,0,180},{-40,-80},{40,0})
+	self.arrows["right"].actor  = createArrow({0,0,-90},{0,-40},{80,40})
 
 end

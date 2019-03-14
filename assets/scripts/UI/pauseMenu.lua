df8a350de18222040b7241902d1e550cea79b5af
Maks <max.maisak@gmail.com>
2019-03-14 12:32:23 +0100

    UI: made the pause menu use UIUtilities

--- assets/scripts/UI/pauseMenu.lua
+++ assets/scripts/UI/pauseMenu.lua
@@ -1,4 +1,5 @@
 require('assets/scripts/object')
+require('assets/scripts/Utility/uiUtilities')
 
 PauseMenu = Object:new()
 local pauseCanvas
@@ -6,27 +7,6 @@ local pauseCanvas
 function PauseMenu:init()	
 	self:createPanel()
 	self.pauseMenuPanel:get("UIRect").isEnabled = false
-end
-
-local function keepAspectRatio(actor , theight)
-
-	local textureSize = actor:get("Sprite").textureSize
-
-	ratio = textureSize.x/textureSize.y
-	local height = theight
-	local width = height * ratio
-	local minWidth = (width / 2) * -1
-	local minHeight =  (height / 2) * -1
-	actor:get("UIRect").offsetMin = { minWidth, minHeight }
-	actor:get("UIRect").offsetMax = { width / 2, height / 2}
-end
-
-local function playSoundObject(filepath, offset, loop, volume)
-	local music = Game.audio.getSound(filepath)
-	music.playingOffset = music.duration * offset
-    music.loop = loop
-    music.volume = volume
-    music:play()
 end
 
 function PauseMenu:createPanel()	
@@ -78,95 +58,19 @@ function PauseMenu:createPanel()
         }
 	}
 
-	self.resumeButton = Game.makeActor {
-		Name = "ResumeButton",
-		Transform = {
-			scale  = {1,1,1},
-			parent = "PauseMenuPanel"
-		},
-		Text = {
-			font   = "fonts/arcadianRunes.ttf",
-			fontSize = 40,
-			color  = {0, 0, 0, 1},
-			string = "Resume"
-        },
-		UIRect = {
-			anchorMin = {0.5, 0.6},
-			anchorMax = {0.5, 0.6}
-		},
-		Sprite = {
-			material = {
-				shader	= "sprite",
-				texture	= "textures/buttonBackground.png",
-			}
-		},
-		LuaBehavior = {
-			onMouseDown = function(self, button)
-				if button == 1 then
-					playSoundObject('audio/UIButtonSound.wav',0,false,60)
-					pauseCanvas:get("UIRect").isEnabled = false
-					self.player.canControl = true
-				end
-			end,
+	self.resumeButton = UIUtilities.makeButton("ResumeButton", "PauseMenuPanel", "Resume", {0.5, 0.6}, {0.5, 0.6}, "textures/buttonBackground.png", function(self)
+		pauseCanvas:get("UIRect").isEnabled = false
+		self.player.canControl = true
+	end)
 
-			--Mouse Over Start
-			onMouseEnter = function(self, button)
-				self.actor:get("Transform").scale = {1.2,1.2,1.2}
-			end,
-
-			onMouseLeave = function(self, button)
-				self.actor:get("Transform").scale = {1,1,1}
-			end
-			--Mouse Over End
-		}
-	}
-
-	local mainMenuButton = Game.makeActor {
-		Name = "MainMenuButton",
-		Transform = {
-			scale  = {1,1,1},
-			parent = "PauseMenuPanel"
-		},
-		Text = {
-			font   = "fonts/arcadianRunes.ttf",
-			fontSize = 40,
-			color  = {0, 0, 0, 1},
-			string = "Main Menu"
-        },
-		UIRect = {
-			anchorMin = {0.5, 0.5},
-			anchorMax = {0.5, 0.5}
-		},
-		Sprite = {
-			material = {
-				shader	= "sprite",
-				texture	= "textures/buttonBackground.png",
-			}
-		},
-		LuaBehavior = {
-			onMouseDown = function(self, button)
-				if button == 1 then
-					playSoundObject('audio/UIButtonSound.wav',0,false,60)
-					Game.loadScene(Config.startScene)
-				end
-			end,
-
-			--Mouse Over Start
-			onMouseEnter = function(self, button)
-				self.actor:get("Transform").scale = {1.2,1.2,1.2}
-			end,
-
-			onMouseLeave = function(self, button)
-				self.actor:get("Transform").scale = {1,1,1}
-			end
-			--Mouse Over End
-		}
-	}
+	local mainMenuButton = UIUtilities.makeButton("MainMenuButton", "PauseMenuPanel", "Main Menu", {0.5, 0.5}, {0.5, 0.5}, "textures/buttonBackground.png", function(self)
+		Game.loadScene(Config.startScene)
+	end)
 
 	pauseCanvas	= Game.find("PauseMenuPanel")
 
-	keepAspectRatio(mainMenuButton,64)
-	keepAspectRatio(self.resumeButton,64)
+	UIUtilities.keepAspectRatio(mainMenuButton   , 64)
+	UIUtilities.keepAspectRatio(self.resumeButton, 64)
 end
 
 function PauseMenu:activate()

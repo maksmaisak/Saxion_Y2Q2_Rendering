672fef6a0efebc234f028a3a7bdccd6055d09d37
Maks <max.maisak@gmail.com>
2019-03-15 01:04:14 +0100

    LevelGoal: added level goal fire flicker

--- assets/scripts/levelGoal.lua
+++ assets/scripts/levelGoal.lua
@@ -6,17 +6,47 @@ require('assets/scripts/player')
 
 LevelGoal = Object:new()
 
+local function startFireFlickering(self)
+    
+    self.goal.light.actor:tweenComplete()
+    local baselineIntensity = self.goal.light.initialIntensity
+    local minIntensity = baselineIntensity * 0.4
+    local maxIntensity = baselineIntensity * 1.6
+    self.goal.light.light.intensity = minIntensity
+    self.goal.light.light:tweenIntensity(maxIntensity, 2, Ease.fluctuate):setLoopBounce()
+
+    self.isFireFlickering = true
+end
+
 function LevelGoal:start()
 
-    self.timer = 0
+    self.isFireActive = self.goal.startActive
+    self.timeOfFireActivation = self.isFireActive and Game.getTime() or 0
+
+    if self.isFireActive then
+        startFireFlickering(self)
+    else
+        self.isFireFlickering = false
+    end
 end
 
 function LevelGoal:update(dt)
 
-    self.timer = self.timer + dt
+    if self.isFireActive and not self.isFireFlickering then
+
+        if Game.getTime() > self.timeOfFireActivation + 1 then
+
+            startFireFlickering(self)
+        end
+    end
 end
 
 function LevelGoal:activateFire()
+
+    if self.isFireActive then return end
+
+    self.isFireActive = true
+    self.timeOfFireActivation = Game.getTime()
 
     self.goal.light.actor:tweenKill()
     self.goal.light.light:tweenIntensity(self.goal.light.initialIntensity, 2, Ease.outExpo)
@@ -26,6 +56,11 @@ function LevelGoal:activateFire()
 end
 
 function LevelGoal:deactivateFire()
+
+    if not self.isFireActive then return end
+
+    self.isFireActive = false
+    self.isFireFlickering = false
 
     self.goal.light.actor:tweenKill()
     self.goal.light.light:tweenIntensity(0, 0.8, Ease.outExpo)
